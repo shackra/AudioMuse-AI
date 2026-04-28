2d3bea837b8cd40525bf31acb44527aca60f5379
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-28 13:44:52 -0600

    Fill the correct SHA256 for each model

--- nix/models.nix
+++ nix/models.nix
@@ -15,27 +15,27 @@ stdenvNoCC.mkDerivation {
   srcs = [
     (fetchurl {
       url = "${baseUrl}/musicnn_embedding.onnx";
-      hash = lib.fakeHash;
+      hash = "sha256-pIrYh5UKVXrvu03N31itSAIhP/X8b7Ue2jUHzXl7ubA=";
       name = "musicnn_embedding.onnx";
     })
     (fetchurl {
       url = "${baseUrl}/musicnn_prediction.onnx";
-      hash = lib.fakeHash;
+      hash = "sha256-DU543UPGEK7IjAmeQfSolpeX2lrCYS6myiH6qeGkKPM=";
       name = "musicnn_prediction.onnx";
     })
     (fetchurl {
       url = "${dclapUrl}/model_epoch_36.onnx";
-      hash = lib.fakeHash;
+      hash = "sha256-F4YEA/j8kK/4rAYyoHQeteWNjAsK0vzlztlnJ0sOqXE=";
       name = "model_epoch_36.onnx";
     })
     (fetchurl {
       url = "${dclapUrl}/model_epoch_36.onnx.data";
-      hash = lib.fakeHash;
+      hash = "sha256-KnNbI8Kq17Etn/yFM0zrzGWcB2ltL/YOLjeNoott9lc=";
       name = "model_epoch_36.onnx.data";
     })
     (fetchurl {
       url = "${baseUrl}/clap_text_model.onnx";
-      hash = lib.fakeHash;
+      hash = "sha256-IA1I85Bf8fJyr1AG3ZhR+UBxp93k6v2cB7wJxaxlpxQ=";
       name = "clap_text_model.onnx";
     })
   ];
